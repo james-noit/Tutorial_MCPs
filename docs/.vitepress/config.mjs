e390a3dc5c42f0a17e16c9a2ebb04965f6670a0d
von.sancho <von.sancho@gmail.com>
2026-02-15 14:29:52 +0000

    Front. Incluido BuyMeACoffee

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
+      { text: '¿Quieres apoyar a tutorialMCP? Invítame a un café ☕', link: 'https://www.buymeacoffee.com/jamesnoitt' }
     ],
 
     sidebar: [
